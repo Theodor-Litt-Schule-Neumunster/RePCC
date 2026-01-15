33e00eaad5bd694a3cd0846970fe3e77438c5b39
Mark <mark100smirnow@gmail.com>
2026-01-15 20:17:24 +0100

    Added base functions for laserpointer [WIP] and added ping request to main.py to test out API calls. Updated req.txt. Started documentation in code with docstrings.

--- Backend/windows/main.py
+++ Backend/windows/main.py
@@ -1,15 +1,34 @@
+import os
 import fastapi
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-App = FastAPI(debug=True)
+os.system("cls")
 
 print("Imports")
 
-@App.get("/")
+# Not used as of now. Will be used as a IP whitelist
+origins = [
+    "http://127.0.0.1:15248"
+]
+
+App = FastAPI(debug=True)
+
+App.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Change later to IP whitelist          
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@App.get("/ping")
 async def root():
-    return {"message":"Hallo"}
+    return {"message":"Success"}
 
 if __name__ == "__main__":
-    uvicorn.run(App, host="0.0.0.0", port=15248)
+    print("Running main")
+    uvicorn.run(App, host="127.0.0.1", port=15248)
