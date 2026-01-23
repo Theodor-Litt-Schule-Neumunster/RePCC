b406d0e44a849956bfeacd039dd415098075d1dc
Mark <mark100smirnow@gmail.com>
2026-01-23 21:49:08 +0100

    Refactor argument parsing in pcmac.py and enhance file handling for read/save modes

--- Backend/windows/pcmac.py
+++ Backend/windows/pcmac.py
@@ -10,31 +10,38 @@ import subprocess
 
 from pyfiglet import figlet_format
 
+APPDATA = os.path.expanduser(os.getenv("USERPROFILE")) + "\\AppData\\Roaming" # type: ignore[attr-defined]
+HEADER = ".RePCC"
+MACDATA = APPDATA + "\\" + HEADER
+
+FILEVER = "0.15"
+
+class macro():
+    def __init__(self) -> None:
+        pass
+
+    
+
 def main():
 
-    def saveMacro(JSONData:str, savedFileName:str):
-        try:
-            data = json.loads(JSONData)
-            print(data)
-            print(type(data))
-        except Exception as e:
-            print(e)
+    def initParser():
 
-    def initparser():
         parser = argparse.ArgumentParser(
             description="Reads or saves a macro",
+            epilog="Will not work if no arguments are parsed."
         )
-
+        
         parser.add_argument(
             "-v", "--verbose",
             action="store_true",
             help="Makes the function EXTRA transparent",
+            default="None"
         )
 
         parser.add_argument(
             "-j", "--json",
             type=str,
-            default="{0}",
+            default="None",
             help="JSON format as a string. Macro data in json",
         )
 
@@ -42,26 +49,92 @@ def main():
             "-n", "--name",
             type=str,
             help="If save: saves macro with given name. If read: searches for macro with given name",
+            default="none"
         )
 
         parser.add_argument(
             "-m", "--mode",
             choices=["save", "read"],
-            default="read",
+            default="none",
         )
 
-        return parser.parse_args()
-    
-    parser_args = initparser()
-
-    print(parser_args.mode)
+        parser.add_argument(
+            "-a", "--all",
+            action="store_true",
+            help="Prints out all the availible data, if accepted. Used in: read -a"
+        )
         
+        args = parser.parse_args()
+        return args
+    
+    args = initParser()
 
-APPDATA = os.path.expanduser(os.getenv("USERPROFILE")) + "\\AppData\\Roaming" # type: ignore[attr-defined]
-HEADER = ".RePCC"
-MACDATA = APPDATA + "\\" + HEADER
+    # may the IFs take over this file.
+    # (I apologize in advance.)
 
-FILEVER = "0.15"
+    def printAllFiles(listdir):
+        for i in range(0, len(listdir)):
+            name = listdir[i][:-6]
+            print("    " + str(i+1) + ". " + name)
+    
+    if args.mode == "read": # If we are trying to read a file...
+
+        listdir = os.listdir(MACDATA+"\\macros")
+
+        if len(listdir) == 0: # ... but there are no files availible to read...
+
+            print("No files avalible to read.") # ... then we yell at the user!
+            return
+        
+        if args.name == "none": # ... but no name was set ...
+
+            if args.all: # ... but the user used "all"
+
+                print("Here are all availible files:")
+                printAllFiles(listdir) # we give the user all files
+                return
+            
+            print("No name given to read. Here are the avalible files:") # ... then we yell at the user ...
+            printAllFiles(listdir) # ... and give them all the avalible file names
+            
+            return
+        
+        if not args.name == "none": # ... and the user gave us a name ...
+
+            name = args.name
+
+            if not name[-6:] == ".pcmac": # ... and the parsed name does not end with ".pcmac" ...
+
+                name = name + ".pcmac" # ... we add .pcmac to the end of the name ...
+
+            if name in listdir: # ... and the name is inside the macro directory ...
+
+                data = None
+
+                with open(MACDATA+"\\macros\\"+name, "r") as f: # ... we open the designated file ...
+                    data = f.read()
+                    f.close()
+
+                return data # ... and return the contents
+
+            if not name in listdir: # ... and the name is not inside the macro directory...
+
+                print("File does not exsist. Here are all availible files:") # ... we yell at the user ...
+                printAllFiles(listdir) # ... and give them all the avalible file names
+
+    if args.mode == "save": # If the user is trying to save a macro ...
+        
+        if args.json == "none": # ... but no JSON data was parsed ...
+            print("No JSON string parsed.") # ... we yell at the user.
+            return
+        
+        if not args.json == "none": 
+            ...
+
+            # TODO: Finish intigration of writing new macro
+    
+    if args.mode == "none":
+        print("Must have MODE set.")
 
 if __name__ == "__main__":
 
