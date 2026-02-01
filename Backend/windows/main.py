4937e90c3f79aa82a8c0422cef21a0a1a828936f
Mark <mark100smirnow@gmail.com>
2026-02-02 00:00:38 +0100

    Refactor args.py message, add register.yaml for MAC storage, recreate requirements.txt, implement mDNS connection handling in main.py, and document AutoConnect feature in AutoConnect.md

--- Backend/windows/main.py
+++ Backend/windows/main.py
@@ -1,6 +1,10 @@
 # imports
 
 import os
+import uuid
+import yaml
+import socket
+import random
 import logging
 import uvicorn
 import logging.config
@@ -12,6 +16,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+from zeroconf import Zeroconf, ServiceInfo
+
 # --
 # custom imports
 
@@ -20,7 +26,15 @@ from pcmac import initializePCMAC
 from webrtc import startWebRTCServer
 # --
 
+MAC = ':'.join(('%012X' % uuid.getnode())[i:i+2] for i in range(0, 12, 2))
 ROAMING = os.path.expanduser(os.getenv("USERPROFILE")) + "\\AppData\\Roaming" # type: ignore[attr-defined]
+TWOFACODE = None
+
+def NEW2FA():
+    global TWOFACODE
+    TWOFACODE = random.randint(11111111,99999999)
+
+NEW2FA()
 
 logging.config.dictConfig(LOGGER_CONF)
 logger = logging.getLogger("RePCC")
@@ -43,21 +57,75 @@ async def ping(request:Request):
     # TODO: Add Argparser for manual saving reading opening
     # TODO: Add HTTP requests for saving reading opening
 
+#--------------- mDNS AUTO PAIRING
+
+@App.post("/connect")
+async def repccConnect(request:Request):
+    print("Request to connect")
+    logger.debug(f"main | Recieving new request to connect by {request.client.host}") # type: ignore[attr-defined]
+
+    data = await request.json()
+    print(data)
+    
+    macsYAML = yaml.safe_load(open(ROAMING+"\\.RePCC\\settings\\register.yaml"))
+    print(macsYAML)
+
+    def pair(mac:str, macsYAML:dict):
+
+        macsYAML = macsYAML["MAC"].append(mac)
+        yaml.safe_dump(macsYAML, open(ROAMING+"\\.RePCC\\settings\\register.yaml"))
+        return
+
+    try:
+        if macsYAML["MAC"] == None:
+            if data["2fa"] == TWOFACODE:
+
+                NEW2FA()
+
+    except Exception as e:
+        print(e)
+        return JSONResponse({"Error":str(e)}, status_code=404)
+
+#--------------- SETTINGS 
+
 @App.get("settings/{file}")
 async def getSetting(file:str):
-    
     ...
+
+def registerMDNS(port:int = 15250):
+    
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+
+    serviceinfo = ServiceInfo(
+        "_http._tcp.local.",
+        f"RePCC.{hostname}._http._tcp.local.",
+        addresses=[socket.inet_aton(local_ip)],
+        port=port,
+        properties={
+            "appversion":"0.1 INDEV",
+            "mac":MAC,
+            "2fa":TWOFACODE
+        },
+        server=f"{hostname}.local.",
+    )
+
+    zc = Zeroconf()
+    zc.register_service(serviceinfo)
+    return zc
 
 if __name__ == "__main__":
 
     logger.info("\n\n--------------------START--------------------")
     logger.info("Wakey wakey eggs 'n bakey! Time to run!")
 
+    logger.info("main | Initializing mDNS")
+    mdns = registerMDNS()
 
     logger.info("main | Initializing .pcmac")
-    initializePCMAC()
+    #initializePCMAC()
 
-    startWebRTCServer()
+    #startWebRTCServer()
     logger.info(f"main | WebRTC server started.")
 
     if os.name == "nt":
---
+++ Backend/windows/test/mdnstest.py
@@ -0,0 +1,54 @@
+from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
+
+import uuid
+import requests
+
+class TestListener(ServiceListener):
+
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        print(f"Service {name} updated")
+        self._handle_Service(zc, type_, name)
+
+    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        print(f"Service {name} removed")
+
+    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        print(f"Service {name} added")
+        self._handle_Service(zc, type_, name)
+
+    def _handle_Service(self, zc: Zeroconf, type_:str, name:str):
+        info = zc.get_service_info(type_, name)
+
+        if info and "repcc" in name.lower():
+            print("Found RePCC Service!")
+            print("ATTEMTING CONNECTION")
+
+            devicemac = ':'.join(('%012X' % uuid.getnode())[i:i+2] for i in range(0, 12, 2))
+
+            ip = info.parsed_addresses()[0]
+            props = info.properties
+            twofa = props[b"2fa"].decode('utf-8') # type: ignore
+
+            addr = f"http://{ip}:15248/connect"
+
+            r = requests.post(url=addr, json={
+                "mac":devicemac,
+                "2fa":twofa
+            })
+
+            if r.status_code == 202 or r.status_code == 200:
+                print("OK!")
+            else:
+                print(f"Response is not 202. Code: {r.status_code}")
+
+
+
+# Create a single Zeroconf instance to share
+zeroconf = Zeroconf()
+listener = TestListener()
+browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
+
+try:
+    input("Press enter to exit...\n\n")
+finally:
+    zeroconf.close()
